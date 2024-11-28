bc8d25983d746f2dd90491b2741aaa73ca9fe254
Monjurul Alam <monjurulalam190629@gmail.com>
2024-11-28 23:53:31 +0600

    navbar and footer is completer

---
+++ transport-app/src/app/Component/Shared/Footer.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer bg-[#306d35] text-white p-20">
+                <nav>
+                    <h6 className="text-3xl md:text-3xl border-2 border-gray-300 px-2 py-3 rounded-md ">TransportEase</h6>
+                    <p className='md:w-96 text-justify text-[18px] py-3'>
+                        Transport, owned and operated by Transport Limited, is Bangladesh's largest online ticket destination,
+                        which is committed to making your life convenient, easier and smarter.
+                    </p>
+                    <p className='text-[18px] mt-4 md:mt-4'>Copyright © {new Date().getFullYear()} - All right reserved</p>
+                </nav>
+                <nav>
+                    <h6 className="text-2xl underline">Services</h6>
+                    <div className='text-[18px] gap-2 flex flex-col py-3'>
+                        <a className="link link-hover">Branding</a>
+                        <a className="link link-hover">Design</a>
+                        <a className="link link-hover">Marketing</a>
+                        <a className="link link-hover">Advertisement</a>
+                    </div>
+                </nav>
+                <nav>
+                    <h6 className="text-2xl underline">Quick Links</h6>
+                    <div className='text-[18px] gap-2 flex flex-col py-3'>
+                        <a className="link link-hover">About us</a>
+                        <a className="link link-hover">Contact</a>
+                        <a className="link link-hover">Jobs</a>
+                        <a className="link link-hover">Press kit</a>
+                    </div>
+                </nav>
+                <nav>
+                    <h6 className="text-2xl underline">Legal</h6>
+                    <div className='text-[18px] gap-2 flex flex-col py-3'>
+                        <a className="link link-hover">Terms of use</a>
+                        <a className="link link-hover">Privacy policy</a>
+                        <a className="link link-hover">Cookie policy</a>
+                    </div>
+                   
+
+                </nav>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
--- transport-app/src/app/Component/Shared/Navbar.jsx
+++ transport-app/src/app/Component/Shared/Navbar.jsx
@@ -1,9 +1,91 @@
-import React from 'react';
+"use client";
+
+import React, { useState } from "react";
+import Link from "next/link";
+import { FaUserCircle } from "react-icons/fa";
 
 const Navbar = () => {
+    const navLink = (
+        <>
+            <li>
+                <Link href="/">Home</Link>
+            </li>
+            <li>
+                <Link href="/detection">Detection</Link>
+            </li>
+        </>
+    );
+
+    const handleLogout = () => {
+        logOut()
+            .then(() => console.log("Log out successful"))
+            .catch((error) => console.log(error.message));
+    };
+
+    const [isHovered, setIsHovered] = useState(false);
+    const handleMouseEnter = () => setIsHovered(true);
+    const handleMouseLeave = () => setIsHovered(false);
+
     return (
-        <div>
-            <p className='text-3xl text-emerald-700'>Hello world</p>
+        <div className="mx-10 py-1 text-green-800">
+            <div className="navbar">
+                {/* Navbar Start */}
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div
+                            tabIndex={0}
+                            role="button"
+                            className="btn btn-ghost lg:hidden"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16"
+                                />
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content z-20 mt-3 shadow border-white border-2 rounded-box w-52"
+                        >
+                            <div>{navLink}</div>
+
+                        </ul>
+                    </div>
+                    <div className="flex lg:hidden justify-center items-center w-[200px]">
+                        
+                        <p className="text-xl md:text-xl text-green-700 font-bold font-serif">TransportEase</p>
+                    </div>
+                    <div className="hidden lg:flex justify-center items-center w-[200px]">
+                        <img
+                            src="https://clipartcraft.com/images/bus-clipart-green-4.png"
+                            className="w-16"
+                            alt="Logo"
+                        />
+                        <p className="text-xl ml-3 md:text-2xl font-bold font-serif text-green-700">TransportEase</p>
+                    </div>
+                </div>
+
+                {/* Navbar Center */}
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-5 py-2 items-center space-x-1">
+                        {navLink}
+                    </ul>
+                </div>
+
+                {/* Navbar End */}
+                <div className="navbar-end">
+                    <FaUserCircle />
+                </div>
+            </div>
         </div>
     );
 };
