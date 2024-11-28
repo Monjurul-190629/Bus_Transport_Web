aacd62d228ea4a02f0391ecbd291feca71e24cee
Monjurul Alam <monjurulalam190629@gmail.com>
2024-11-28 22:30:06 +0600

    first commit

---
+++ transport-app/src/app/Component/Home/Home1.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home1 = () => {
+    return (
+        <div className='text-2xl text-blue-700'>
+            This is my HOme
+        </div>
+    );
+};
+
+export default Home1;
---
+++ transport-app/src/app/Component/Shared/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <p className='text-3xl text-emerald-700'>Hello world</p>
+        </div>
+    );
+};
+
+export default Navbar;
